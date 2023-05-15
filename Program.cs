ff5dba40b280dc338cac9965749c1ac60a37b78c
MukulDevPatel <125425636+MukulDevPatel@users.noreply.github.com>
2023-05-15 20:39:25 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@
             Console.WriteLine("LinkedList Data Structure");
 
             LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
             linkedList.Add(70);
+            linkedList.Add(30);
+            linkedList.Add(56);
             linkedList.Display();
         }
     }
