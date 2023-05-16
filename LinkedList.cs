3274e77d2682a16fadaa08316d455ee074c47fee
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-16 19:14:57 +0530

    [Mukul Dev] Add: We delete target integer value and find the size of integer

--- LinkedList.cs
+++ LinkedList.cs
@@ -54,7 +54,40 @@ namespace LinkedListDataStructure
             }
         }
 
+        public void Remove(int targetValue)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("There is no value");
+            }
+            if (head.data  == targetValue)
+            {
+                head = head.next;
+                return;
+            }
+            Node currentNode = head;
+            while (currentNode.next != null)
+            {
+                if (currentNode.next.data == targetValue)
+                {
+                    currentNode.next = currentNode.next.next;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+        }
 
+        public int Size()
+        {
+            int count = 0;
+            Node current = head;
+            while (current != null)
+            {
+                count++;
+                current = current.next;
+            }
+            return count;
+        }
 
         public void Display()
         {
--- Program.cs
+++ Program.cs
@@ -13,11 +13,22 @@ namespace LinkedListDataStructure
             linkedList.Add(70);
             linkedList.Display();
 
+            //For insertion process
             int targetValue = 30;
             int newData = 40;
             linkedList.InsertNumAtParticularPosition(targetValue, newData);
             Console.WriteLine("Insert value {1} after {0} value.",targetValue,newData);
             linkedList.Display();
+
+            //For deletion process
+            int deletionVal = 40;
+            linkedList.Remove(deletionVal);
+            Console.WriteLine("Remove From the list: "+deletionVal);
+            linkedList.Display();
+
+            //For check size of list
+            int elementSize = linkedList.Size();
+            Console.WriteLine("Size of integer value: {0}", elementSize);
         }
     }
 }
